00afa194f8ddded1b4c996f61f4b684040e7aecd
Ben Strathdee <ben.strathdee@live.com.au>
2022-03-10 13:38:14 +1100

    added project images for remaining projects

--- src/images/index.js
+++ src/images/index.js
@@ -1,13 +1,19 @@
 import OldPortfolio from "./project_images/old_portfolio.jpg";
 import HTMLCalculator from "./project_images/html_calculator.png";
 import MorseCode from "./project_images/morse_code.png";
-import JavaCalculator from "./project_images/java_calculator.png";
-import JavaCardGame from "./project_images/java_card_game.png";
+import JavaCalculator from "./project_images/java_calculator.jpg";
+import JavaCardGame from "./project_images/java_card_game.jpg";
+import CommerceSite from "./project_images/commerce_site.jpg";
+import GoogleBooks from "./project_images/google_books.jpg";
+import PokerEval from "./project_images/poker_eval.jpg";
 
 export {
-    OldPortfolio,
-    HTMLCalculator,
-    MorseCode,
-    JavaCalculator,
-    JavaCardGame,
+	OldPortfolio,
+	HTMLCalculator,
+	MorseCode,
+	JavaCalculator,
+	JavaCardGame,
+	CommerceSite,
+	GoogleBooks,
+	PokerEval,
 };
